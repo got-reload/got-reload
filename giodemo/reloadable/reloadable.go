da841a3219a88a496176ccb211b1a3e6dc2f8899
Chris Waldon <christopher.waldon.dev@gmail.com>
2021-02-21 21:07:06 -0500

    demo: add yaegi bug reproducer
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- giodemo/reloadable/reloadable.go
+++ giodemo/reloadable/reloadable.go
@@ -2,6 +2,7 @@ package reloadable
 
 import (
 	"image/color"
+	"log"
 	"math"
 
 	"gioui.org/f32"
@@ -21,17 +22,20 @@ var (
 	offset float64
 )
 
-const increment = 0.0001
+const increment = 0.001
 
 func Layout(gtx C, th *material.Theme) D {
 	return layout.Center.Layout(gtx, func(gtx C) D {
 		offset += increment
+		log.Println("offset", offset)
 		multiplier := float32(math.Sin(offset))
+		log.Println("sin(offset)", multiplier)
 		final := multiplier * float32(gtx.Constraints.Max.Y/4)
+		log.Println("final", final)
 		op.Offset(f32.Pt(0, final)).Add(gtx.Ops)
 		op.InvalidateOp{}.Add(gtx.Ops)
 		l := material.Editor(th, &ed, "Hello live world!")
-		textColor := color.NRGBA{R: 0, G: 200, B: 0, A: 255}
+		textColor := color.NRGBA{R: 00, G: 200, B: 200, A: 255}
 		l.Color = textColor
 		return l.Layout(gtx)
 	})
